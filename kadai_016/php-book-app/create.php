878ad15016ea95604d89360feaceb3f8632c1870
nuykmk <yy.uni.yy.yy@gmail.com>
2025-08-01 11:02:06 +0900

    データベース情報の修正

--- kadai_016/php-book-app/create.php
+++ kadai_016/php-book-app/create.php
@@ -1,9 +1,8 @@
 <?php
-$dsn = 'mysql:dbname=yx8nwp8vjv54wax9;host=am1shyeyqbxzy8gc.cbetxkdyhwsb.us-east-1.rds.amazonaws.com;charset=utf8mb4';
-$user = 'o3i1el0thl2y6ehy';
-$password = 'qpzv2zo8ic3a3ll2';
-// mysql: //o3i1el0thl2y6ehy:qpzv2zo8ic3a3ll2@am1shyeyqbxzy8gc.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/yx8nwp8vjv54wax9
-
+$dsn = 'mysql:dbname=e26gglq2f8je6dvj;host=m1shyeyqbxzy8gc.cbetxkdyhwsb.us-east-1.rds.amazonaws.com;charset=utf8mb4';
+$user = 'lj6ecx8h4ys6o0dk';
+$password = 'nr0sr1of8azdndu8';
+// mysql: //lj6ecx8h4ys6o0dk:nr0sr1of8azdndu8@am1shyeyqbxzy8gc.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/e26gglq2f8je6dvj
 if (isset($_POST['submit'])) {
   try {
     $pdo = new PDO($dsn, $user, $password);
